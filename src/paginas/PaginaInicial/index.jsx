3f8239a7b48eb308149a84aa38a87dd936a0c70d
DevRuiz18 <gustavosruiz@gmail.com>
2023-01-21 21:06:14 -0300

    inital commit

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,7 +1,22 @@
 import Header from "../../components/Header";
+import Menu from "../../components/Menu";
+import banner from "./banner.png";
+
+import styles from './PaginaInicial.module.scss';
 
 export default function PaginaInicial() {
     return(
-        <Header/>
+        <>
+            <Header/>
+            <main>
+                <section className={styles.principal}>
+                    <Menu/>
+                    <div className={styles.principal__imagem}>
+                        <h1>A galeria mais completa do espaço</h1>
+                        <img src={banner} alt="A imagem da terra vista do espaço"></img>
+                    </div>
+                </section>
+            </main>
+        </>
     );
 }
