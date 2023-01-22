b2117c63a7e42491fb34481d3536985d0d635bd0
DevRuiz18 <gustavosruiz@gmail.com>
2023-01-22 11:20:43 -0300

    filters done

--- src/components/Tags/index.jsx
+++ src/components/Tags/index.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-
+import fotos from "../Galeria/fotos.json";
 import styles from './Tags.module.scss'
 
-export default function Tags() {
+export default function Tags({tags, filtraFotos, setItens}) {
     return(
         <div className={styles.tags}>
-            <p>Filtre por tags:</p>
-            <ul className={styles.tags__lista}>
-                <li>Estrelas</li>
-                <li>Galáxias</li>
-                <li>Lua</li>
-                <li>Planetas</li>
-            </ul>
-        </div>
+        <p>Filtre por tags:</p>
+        <ul className={styles.tags__lista}>
+          {tags.map((tag) => {
+            return (
+              <li key={tag} onClick={() => filtraFotos(tag)}>
+                {tag}
+              </li>
+            );
+          })}
+          <li onClick={() => setItens(fotos)}>Todas</li>
+        </ul>
+      </div>
     );
 }
